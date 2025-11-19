e29ed45dc88f6030efd5b6738913246a127c0d78
chengmin <chengmin@qq.com>
2025-11-19 18:11:48 +0800

    80

--- mobile-website/src/views/Home.vue
+++ mobile-website/src/views/Home.vue
@@ -11,24 +11,34 @@
         <button @click="logout">退出</button>
       </div>
     </header> -->
-    
+
     <main class="main-content">
-      <div class="module-grid">
-        <div class="module-card" @click="goToModule('star-profile')">
-          <h3>明星档案</h3>
-          <p>查看明星详细资料</p>
+      <div class="main-container">
+        <div class="module-grid">
+          <div class="module-card" @click="goToModule('star-profile')">
+            <div class=" module-card1"></div>
+            <h3>明星档案</h3>
+            <p>详细资料</p>
+          </div>
+          <div class="module-card" @click="goToModule('data-workshop')">
+            <div class=" module-card2"></div>
+            <h3>数据工坊</h3>
+            <p>分析与可视化</p>
+          </div>
+
         </div>
-        <div class="module-card" @click="goToModule('data-workshop')">
-          <h3>数据工坊</h3>
-          <p>数据分析与可视化</p>
-        </div>
-        <div class="module-card" @click="goToModule('works-promotion')">
-          <h3>作品安利</h3>
-          <p>推荐优秀作品</p>
-        </div>
-        <div class="module-card" @click="goToModule('fan-voice')">
-          <h3>粉丝心声</h3>
-          <p>粉丝互动交流区</p>
+        <div class="module-grid">
+
+          <div class="module-card" @click="goToModule('works-promotion')">
+            <div class=" module-card3"></div>
+            <h3>作品安利</h3>
+            <p>优秀作品</p>
+          </div>
+          <div class="module-card" @click="goToModule('fan-voice')">
+            <div class=" module-card4"></div>
+            <h3>粉丝心声</h3>
+            <p>互动交流区</p>
+          </div>
         </div>
       </div>
     </main>
@@ -68,7 +78,8 @@ export default {
   align-items: center;
 }
 
-.auth-buttons button, .user-info button {
+.auth-buttons button,
+.user-info button {
   margin-left: 10px;
   padding: 5px 10px;
   border: none;
@@ -79,27 +90,86 @@ export default {
 }
 
 .main-content {
-  padding: 1rem;
+
+  height: 100vh;
+  background-image: url('../assets/image/bg-home.jpg');
+  background-size: cover;
+  position: relative;
 }
 
 .module-grid {
-  display: grid;
-  grid-template-columns: repeat(2, 1fr);
-  gap: 1rem;
+  display: flex;
+  /* flex-direction: column; */
+  align-items: center;
+  justify-content: space-between;
+  margin: 30px;
+
+}
+
+.main-container {
+  position: absolute;
+  bottom: 0;
+  width: 100%;
+  padding: 1rem;
+}
+
+.module-grid1 {
+  display: flex;
+  /* flex-direction: column; */
+  align-items: center;
+  justify-content: space-between;
+  /* margin: 30px; */
+  position: absolute;
+  bottom: 300px;
 }
 
 .module-card {
-  background-color: #f5f5f5;
-  border-radius: 8px;
+  /* background-color: #f5f5f5; */
+  background: linear-gradient(135deg, #ff9a9e 0%, #fad0c4 100%);
+  border-radius: 30px;
   padding: 1rem;
   text-align: center;
   cursor: pointer;
   transition: transform 0.2s;
+  position: relative;
+  width: 45%;
+}
+
+.module-card1 {
+  margin: 5px auto;
+  width: 50px;
+  height: 50px;
+  background-image: url(../assets/icon/1.png);
+  background-size: cover;
+}
+
+.module-card3 {
+  margin: 5px auto;
+  width: 50px;
+  height: 50px;
+  background-image: url(../assets/icon/3.png);
+  background-size: cover;
+}
+
+.module-card2 {
+  margin: 5px auto;
+  width: 50px;
+  height: 50px;
+  background-image: url(../assets/icon/2.png);
+  background-size: cover;
+}
+
+.module-card4 {
+  margin: 5px auto;
+  width: 50px;
+  height: 50px;
+  background-image: url(../assets/icon/4.png);
+  background-size: cover;
 }
 
 .module-card:hover {
   transform: translateY(-5px);
-  box-shadow: 0 4px 8px rgba(0,0,0,0.1);
+  box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
 }
 
 .module-card h3 {
